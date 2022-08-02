01a97a0e8a9c0974c9b3a8f0617a92665cf6ac72
max <max.trimnell@capgemini.com>
2022-08-02 15:09:26 +0100

    @

--- project3/Main.java
+++ project3/Main.java
@@ -1,7 +1,15 @@
 package project3;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("hello");
+
+    public static void main(String[] args)
+    {
+        int [] arr = new int [7]; // 7 is the size . default to 7 zero
+        //or
+        int []  bee = {1, 2,3,4,5} ;
     }
+
+
 }
